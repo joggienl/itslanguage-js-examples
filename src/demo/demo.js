5ff3fa241ca5941a098fde1b9fef50e656542bb3
Lehren <loek-ehren@hotmail.com>
2016-11-07 14:26:05 +0100

    feat(demo): remove functionality hiding code
    Remove extra code relating to hiding tabs.

--- src/demo/demo.js
+++ src/demo/demo.js
@@ -313,9 +313,6 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   };
 
-    recognitionButton.parentElement.classList.remove('hidden');
-    analysisButton.parentElement.classList.remove('hidden');
-
   function toggleOn(kind) {
     const button = document.getElementById(kind + 'Button');
     button.parentElement.classList.add('active');
